3746d08c05b3b5397dd54450fa0ca3fe8fd43443
middletaq@gmail.com <middletaq@gmail.com>
2023-05-17 19:22:18 -0400

    demo

--- app/routes.js
+++ app/routes.js
@@ -12,29 +12,59 @@ module.exports = function (app, passport, db) {
 
   // Intake route after signup this forwards us to the correct intake form
   app.get('/intake', isLoggedIn, function (req, res) {
-    if(req.user.role === 'coach'){
+    if (req.user.role === 'coach') {
       res.redirect('/intakeCoach')
-    }else{
+    } else {
       res.redirect('/intakeAthlete')
     }
 
   });
 
-  app.get('/chatHome', isLoggedIn, function (req, res) {
-    res.render('chatHome.ejs', {
-      user: req.user
-    })
-      });
+  app.get('/chatHome/:coachId', isLoggedIn, function (req, res) {
+    
+      db.collection('users').findOne({_id: ObjectID(req.user._id)},(err, user) => {
+        if(err){
+          return console.log(err)
+        }
+        db.collection('chatRequest').insertOne({
+          coachId: req.params.coachId,
+          athleteId: req.user._id,
+          chatRoomId: req.params.coachId,
+          athleteName: req.user.firstName,
+          athleteGoals: user.userIntake.goals,
+          athleteSkill: user.userIntake.skill,
+          athleteLevel: user.userIntake.level
+        },(err) => {
+          if(err){
+            return console.log(err)
+          }
+        })
 
-      // this has to change to accept a coach id as a param. app.get('/chatroom:id')
+        
+        res.render('chatHome.ejs', {
+          user: req.user,
+          coachName: req.params.coachId
+        });
+
+      })
+
+  });
+ 
+
+  // db.collections('chatRequests).save({coachID: ObjectId(req.params.coachID), athleteId: req.user._id, chatRoomId: req.params.coachId, athleteName: req.user.firstName, athleteGoals: req.user.userIntake.goals, athleteSkill: req.user.userIntake.skill, athleteLevel: req.user.userIntake.level}, (err, result) => {
+  //   if (err) return console.log(err)
+  //   res.redirect("")
+  //   })
+
+  // this has to change to accept a coach id as a param. app.get('/chatroom:id')
   app.get('/chatroom', isLoggedIn, function (req, res) {
     // const coachId = req.params.id
     // use this coach id to find the specific coach in the users collection. something like coach = users.filter((user)=> user._id)
     // log out the value of coach and once we have the correct coach, we just have to pass their name into the chatroom ejs
-res.render('chatroom.ejs', {
-  user: req.user
-  // coach
-})
+    res.render('chatroom.ejs', {
+      user: req.user
+      // coach
+    })
   });
 
 
@@ -47,33 +77,33 @@ res.render('chatroom.ejs', {
   });
 
   // Intake form for coach after signup
-  
-  app.get('/intakeCoach', isLoggedIn, function (req, res) {
-    
-      res.render('intakeCoach.ejs',{
-        user: req.user
-      })
-      
-  });
-  
-  // Rest of the code...
-  
 
-  
+  app.get('/intakeCoach', isLoggedIn, function (req, res) {
+
+    res.render('intakeCoach.ejs', {
+      user: req.user
+    })
+
+  });
+
+  // Rest of the code...
+
+
+
 
   // PROFILE SECTION =========================
-  app.get('/coach', isLoggedIn, function (req, res) {
-    db.collection('completedWorkouts').find().toArray((err, result) => {
-      if (err) return console.log(err)
-      res.render('coach.ejs', {
-        user: req.user,
-        workout: result
-      })
-    })
-  });
+  // app.get('/coach', isLoggedIn, function (req, res) {
+  //   db.collection('completedWorkouts').find().toArray((err, result) => {
+  //     if (err) return console.log(err)
+  //     res.render('coach.ejs', {
+  //       user: req.user,
+  //       workout: result
+  //     })
+  //   })
+  // });
   app.get('/profile', isLoggedIn, function (req, res) {
     if (req.user.role === 'coach') {
-      res.redirect('/coach');
+      res.redirect('/coachPage');
       return
     }
 
@@ -99,7 +129,7 @@ res.render('chatroom.ejs', {
   app.post('/improve', (req, res) => {
     // console.log(req.body);
     // console.log('this logged in user ', req.user);
-  
+
     db.collection('users').findOneAndUpdate(
       { _id: ObjectID(req.user._id) },
       {
@@ -117,21 +147,21 @@ res.render('chatroom.ejs', {
           console.log(err);
           return res.redirect('/profile');
         }
-  
+
         // Retrieve updated user data
         db.collection('users').findOne({ _id: ObjectID(req.user._id) }, (err, updatedUser) => {
           if (err) {
             console.log(err);
             return res.redirect('/profile');
           }
-  
+
           console.log(updatedUser);
-  
+
           // Check if the user wants to work with a coach
           if (updatedUser.userIntake.workWithCoach === 'yes') {
             return res.redirect('/coachList');
           }
-  
+
           res.redirect('/profile');
         });
       }
@@ -143,7 +173,7 @@ res.render('chatroom.ejs', {
   app.post('/intakeCoach', (req, res) => {
     // console.log(req.body);
     // console.log('this logged in user ', req.user);
-  
+
     db.collection('users').findOneAndUpdate(
       { _id: ObjectID(req.user._id) },
       {
@@ -162,45 +192,58 @@ res.render('chatroom.ejs', {
           console.log(err);
           return res.redirect('/profile');
         }
-  
+
         // Retrieve updated user data
         db.collection('users').findOne({ _id: ObjectID(req.user._id) }, (err, updatedUser) => {
           if (err) {
             console.log(err);
             return res.redirect('/profile');
           }
-  
+
           // console.log(updatedUser);
-  
-        
-  
+
+
+
           res.redirect('/profile');
         });
       }
     );
   });
-  
-    // db.collection('workout').find({level: req.body.level, skill: req.body.skill}).toArray((err, result) => {
-    //   if (err) return console.log(err)
-    //   res.render('profile.ejs', {
-    //     user : req.user,
-    //     workout: result
-    //   })
-    // })
-  
-    app.get('/coachList', function(req, res) {
-      db.collection('users').find().toArray((err, users) => {
-        console.log({users})
-        const coaches = users.filter((user)=> user.role === 'coach')
-        if (err) return console.log(err);
-        res.render('coachList.ejs', { coaches });
-      });
+
+  // db.collection('workout').find({level: req.body.level, skill: req.body.skill}).toArray((err, result) => {
+  //   if (err) return console.log(err)
+  //   res.render('profile.ejs', {
+  //     user : req.user,
+  //     workout: result
+  //   })
+  // })
+
+  app.get('/coachList', function (req, res) {
+    db.collection('users').find().toArray((err, users) => {
+      console.log({ users })
+      const coaches = users.filter((user) => user.role === 'coach')
+      if (err) return console.log(err);
+      res.render('coachList.ejs', { coaches });
     });
-    
-    
-    
-    
-    
+  });
+
+
+  app.get('/coachPage', function (req, res) {
+    db.collection('chatRequest').find().toArray((err, users) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send('Internal Server Error');
+      }
+
+      const incomingChatRequests = users.filter((user) => user.userIntake && user.userIntake.level);
+
+      res.render('coach.ejs', { incomingChatRequests });
+    });
+  });
+
+
+
+
 
 
 
